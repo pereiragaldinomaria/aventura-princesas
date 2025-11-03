439f20b920c9ea961d9e959f6e70a96c3496b902
pereiragaldinomaria <pereira.galdino.maria@escola.pr.gov.br>
2025-11-03 10:04:20 -0300

    ç

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListener('click', function(){
-
+    const atual = document.querySelector('.ativo')
+    const proximoPasso = "passo" + this.getAttribute('data-proximo')
+    atual.classList.remove('ativo')
+    document.getElementById(proximoPasso).classList.add('ativo')
     })
-})
+}
+
+)
